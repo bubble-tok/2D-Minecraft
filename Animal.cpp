e91f2cbe26c378fc604dbec721a5b8be3afedd9d
Song-Dong Lai <slai249@uwo.ca>
2026-04-02 20:06:59 +0000

    Edit Animal.cpp Comments

--- Animal.cpp
+++ Animal.cpp
@@ -2,6 +2,10 @@
  * @file Animal.cpp
  * @brief Implements Animal wandering physics and meat drop behaviour.
  *
+ * This class represents an animal in the game, storing information about the
+ * health, meat type and amount of meat dropped, as well as managing the
+ * movement and physics of the animal.
+ *
  * @author Group 46
  */
 #include "Animal.h"
@@ -17,7 +21,7 @@ Animal::Animal(float x, float y,
 void Animal::tick(float dt, const TileMap& map) {
     if (!alive) return;
 
-    // Reverse direction every 2 seconds (simple idle wander)
+    // Reverse direction of the animal every 2 seconds (simple idle wandering)
     wanderTimer += dt;
     if (wanderTimer > 2.f) { wanderDir = -wanderDir; wanderTimer = 0.f; }
 
@@ -34,14 +38,14 @@ void Animal::tick(float dt, const TileMap& map) {
     // Resolve collisions against the tile map
     resolveTileCollision(x, y, vx, vy, onGround, map);
 
-    // If horizontal movement was blocked by terrain, reverse direction
+    // If horizontal movement was blocked by terrain, reverse the direction of the animal
     if ((vx > 0.f && x < prevX + vx * dt - 0.5f) ||
         (vx < 0.f && x > prevX + vx * dt + 0.5f))
         wanderDir = -wanderDir;
 }
 
 std::shared_ptr<Food> Animal::dropMeat() const {
-    // Animals configured as CookedMeat drop the cooked variant directly
+    // Animals configured as CookedMeat will drop the cooked variant directly
     if (meatType == "CookedMeat")
         return std::make_shared<Food>(FoodItems::CookedMeat(meatAmount));
     return std::make_shared<Food>(FoodItems::RawMeat(meatAmount));
